734d92300590db42921f44727ab9a53afb271cb0
jimny <jimnychen0826@gmail.com>
2024-03-19 13:10:23 +0800

    hw4_2

---
+++ hw04_2/src/hw04_2/hw4_2.java
@@ -0,0 +1,24 @@
+package hw04_2;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class hw4_2 {
+	public static void main(String[] args) {
+		Path filePath = Path.of("src/demo.txt");
+		try {
+			String text = Files.readString(filePath);
+			Pattern pattern = Pattern.compile("01AF0055\\/\\d{6}\\D.HRV.jpg\\?HRV.bpp");
+			Matcher matcher = pattern.matcher(text);
+			while(matcher.find()) {
+				System.out.println(matcher.group());
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
-set /p commitmessage=input commit message
+set /p commitmessage=input commit message :
 git add .
 git commit -m  %commitmessage%
 git push origin main
