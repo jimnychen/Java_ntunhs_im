3b3b1cb15ef6057b7da80e3fe8f78c5f2383abdb
jimny <jimnychen0826@gmail.com>
2024-05-14 15:18:36 +0800

    hw 11

--- HW11-1/src/App.java
+++ HW11-1/src/App.java
@@ -1,8 +1,14 @@
+import javax.swing.JFrame;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        Jframe frame = new Jframe()
+        JFrame window =new JFrame();
+        window.setSize(300,300);
+        window.setBounds(500,500,400,300);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setTitle("brod");
+        window.setVisible(true);
     }
     
 }
